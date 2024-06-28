b86c84a606ef05fb97e5cc26aeca28790ba80107
abhinav-dev007 <dasabhinav00711200@gmail.com>
2024-06-29 00:22:50 +0530

    first commit

---
+++ lib/Controller/services/API/apis.dart
@@ -0,0 +1,3 @@
+String cryptoMarketDataAPI = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=$currency&per_page=$noofData&x_cg_demo_api_key=CG-iGsMbwb1aWcpaQYYYQEt4ytQ';
+String currency = 'inr';
+String noofData = '20';
---
+++ lib/View/wishlist_screen.dart
@@ -0,0 +1,56 @@
+import 'package:crypto_tracker/Controller/provider/crypto_provider.dart';
+import 'package:crypto_tracker/Model/crypto_data_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+import 'crypto_listing_screen.dart';
+
+class CryptoWishlistScreen extends StatefulWidget {
+  const CryptoWishlistScreen({super.key});
+
+  @override
+  State<CryptoWishlistScreen> createState() => _CryptoWishlistScreenState();
+}
+
+class _CryptoWishlistScreenState extends State<CryptoWishlistScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CryptoDataProvider>(
+      builder: (context, marketProvider, child) {
+        List<CryptoDataModel> wishlist = marketProvider.cryptoData
+            .where((element) => element.addedToWishlist == true)
+            .toList();
+
+        if (wishlist.isNotEmpty) {
+          return RefreshIndicator(
+            onRefresh: () async {
+              await marketProvider.fetchData();
+            },
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(
+                horizontal: 5.w,
+              ),
+              physics: const BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              itemCount: wishlist.length,
+              itemBuilder: (context, index) {
+                CryptoDataModel currentCrypto = wishlist[index];
+                return CryptoTile(currentCryptoData: currentCrypto);
+              },
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text(
+              "No CryptoCurrency added to Wishlist",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
